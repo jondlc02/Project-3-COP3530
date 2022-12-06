e2f0f979c3d3e3e28cbb760a65e00080dcf16c69
Andrea <andrea.moreno223@gmail.com>
2022-12-06 15:50:26 -0500

    updated main

--- main.cpp
+++ main.cpp
@@ -31,6 +31,7 @@ int main()
         switch (userOption)
         {
         case 1:
+        {
             double min;
             double max;
             cout << "What is the minimum temperature you desire?" << endl;
@@ -38,23 +39,41 @@ int main()
             cout << "What is the maximum temperature you desire?" << endl;
             cin >> max;
 
+            startUnordered = clock();
+            vector<pair<int, string>> resultsUnordered = unordered.avgTemp(min, max);
+            endUnordered = clock();
+            cout << "Your optimal location(s) are: " << endl;
+            for (int i = 0; i < resultsUnordered.size(); i++)
+            {
+                if (i == 0)
+                {
+                    cout << resultsUnordered[i].second << " ";
+                }
+                else if (resultsUnordered[i].first == resultsUnordered[0].first)
+                {
+                    cout << resultsUnordered[i].second << " ";
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            // ordered
             startOrdered = clock();
-            ordered.avgTemp(min, max);
+            vector<pair<int, string>> resultsOrdered = ordered.avgTemp(min, max);
             endOrdered = clock();
 
-            startUnordered = clock();
-            unordered.avgTemp(min, max);
-            endUnordered = clock();
-
-            cout << "Time of Ordered Map: "
-                 << (endOrdered - startOrdered) / (float)CLOCKS_PER_SEC << " seconds"
-                 << endl;
             cout << "Time of Unordered Map: "
                  << (endUnordered - startUnordered) / (float)CLOCKS_PER_SEC
                  << " seconds" << endl;
+            cout << "Time of Ordered Map: "
+                 << (endOrdered - startOrdered) / (float)CLOCKS_PER_SEC << " seconds"
+                 << endl;
             menu = false;
 
             break;
+        }
         case 2:
         {
 
@@ -62,37 +81,44 @@ int main()
             cout << "What is the minumum temperature you desire?" << endl;
             cin >> minTemperature;
 
-            startOrdered = clock();
-            vector<pair<int, string>> results = unordered.minTemp(50);
+            startUnordered = clock();
+            vector<pair<int, string>> resultsUnordered = unordered.minTemp(minTemperature);
+            endUnordered = clock();
 
             cout << "Your optimal location(s) are: " << endl;
 
             // this should print out first element of vector and those with the same
             // freq. as it
-            /* for (int i = 0; i < results.size(); i++) {
-              if (i == 0) {
-                cout << results[i].second << endl;
-              } else if (results[i].first == results[0].first) {
-                cout << results[i].second << endl;
-              } else {
-                break;
-              }
-            } */
+            for (int i = 0; i < resultsUnordered.size(); i++)
+            {
+                if (i == 0)
+                {
+                    cout << resultsUnordered[i].second << " ";
+                }
+                else if (resultsUnordered[i].first == resultsUnordered[0].first)
+                {
+                    cout << resultsUnordered[i].second << " ";
+                }
+                else
+                {
+                    break;
+                }
+            }
 
-            // this is just to test
-            cout << results[0].second << endl;
+            /* this is just to test
+            cout << results[0].second << endl; */
+
+            // ordered
+            startOrdered = clock();
+            vector<pair<int, string>> resultsOrdered = ordered.minTemp(minTemperature);
             endOrdered = clock();
 
-            /* startUnordered = clock();
-            unordered.minTemp(minTemperature);
-            endUnordered = clock(); */
-
+            cout << "Time of Unordered Map: "
+                 << (endUnordered - startUnordered) / (float)CLOCKS_PER_SEC
+                 << " seconds" << endl;
             cout << "Time of Ordered Map: "
                  << (endOrdered - startOrdered) / (float)CLOCKS_PER_SEC << " seconds"
                  << endl;
-            /* cout << "Time of Unordered Map: "
-                 << (endUnordered - startUnordered) / (float)CLOCKS_PER_SEC
-                 << " seconds" << endl; */
             menu = false;
 
             break;
@@ -102,38 +128,40 @@ int main()
             double maxTemperature;
             cout << "What is the maximum temperature you desire?" << endl;
             cin >> maxTemperature;
-            vector<pair<int, string>> results;
 
-            startOrdered = clock();
-            ordered.maxTemp(maxTemperature);
+            // unordered
+            startUnordered = clock();
+            vector<pair<int, string>> resultsUnordered = unordered.maxTemp(maxTemperature);
+            endUnordered = clock();
             cout << "Your optimal location(s) are: " << endl;
-            for (int i = 0; i < results.size(); i++)
+            for (int i = 0; i < resultsUnordered.size(); i++)
             {
                 if (i == 0)
                 {
-                    cout << results[i].second << endl;
+                    cout << resultsUnordered[i].second << " ";
                 }
-                if (results[i].first == results[0].first)
+                if (resultsUnordered[i].first == resultsUnordered[0].first)
                 {
-                    cout << results[i].second << endl;
+                    cout << resultsUnordered[i].second << " ";
                 }
                 else
                 {
                     break;
                 }
             }
+
+            // ordered
+
+            startOrdered = clock();
+            vector<pair<int, string>> resultsOrdered = ordered.maxTemp(maxTemperature);
             endOrdered = clock();
 
-            startUnordered = clock();
-            unordered.maxTemp(maxTemperature);
-            endUnordered = clock();
-
-            cout << "Time of Ordered Map: "
-                 << (endOrdered - startOrdered) / (float)CLOCKS_PER_SEC << " seconds"
-                 << endl;
             cout << "Time of Unordered Map: "
                  << (endUnordered - startUnordered) / (float)CLOCKS_PER_SEC
                  << " seconds" << endl;
+            cout << "Time of Ordered Map: "
+                 << (endOrdered - startOrdered) / (float)CLOCKS_PER_SEC << " seconds"
+                 << endl;
             menu = false;
 
             break;
@@ -145,10 +173,55 @@ int main()
             cin >> minwindSpeed;
             cout << "What is the maximum wind speed you desire?" << endl;
             cin >> maxWindSpeed;
-            vector<pair<int, string>> results;
 
+            // unordered
+            startUnordered = clock();
+            vector<pair<int, string>> resultsUnordered = unordered.windSpd(minwindSpeed, maxWindSpeed);
+            endUnordered = clock();
+            cout << "Your optimal location(s) are: " << endl;
+            for (int i = 0; i < resultsUnordered.size(); i++)
+            {
+                if (i == 0)
+                {
+                    cout << resultsUnordered[i].second << " ";
+                }
+                if (resultsUnordered[i].first == resultsUnordered[0].first)
+                {
+                    cout << resultsUnordered[i].second << " ";
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            // ordered
             startOrdered = clock();
-            results = ordered.windSpeed(minwindSpeed, maxWindSpeed);
+            vector<pair<int, string>> resultsOrdered = ordered.windSpeed(minwindSpeed, maxWindSpeed);
+            endOrdered = clock();
+
+            cout << "Time of Unordered Map: "
+                 << (endUnordered - startUnordered) / (float)CLOCKS_PER_SEC
+                 << " seconds" << endl;
+            cout << "Time of Ordered Map: "
+                 << (endOrdered - startOrdered) / (float)CLOCKS_PER_SEC << " seconds"
+                 << endl;
+            menu = false;
+
+            break;
+        }
+        case 5:
+        {
+            double minPrecipitation, maxPrecipitation;
+            cout << "What is the minimum precipitation you desire?" << endl;
+            cin >> minPrecipitation;
+            cout << "What is the maximum precipitation you desire?" << endl;
+            cin >> maxPrecipitation;
+
+            // unordered
+            startUnordered = clock();
+            vector<pair<int, string>> results = unordered.precip(minPrecipitation, maxPrecipitation);
+            endUnordered = clock();
             cout << "Your optimal location(s) are: " << endl;
             for (int i = 0; i < results.size(); i++)
             {
@@ -165,45 +238,21 @@ int main()
                     break;
                 }
             }
+
+            // ordered
+            startOrdered = clock();
+            vector<pair<int, string>> resultsOrdered = ordered.precipitation(minPrecipitation, maxPrecipitation);
             endOrdered = clock();
 
-            startUnordered = clock();
-            unordered.windSpd(minwindSpeed, maxWindSpeed);
-            endUnordered = clock();
-
-            cout << "Time of Ordered Map: "
-                 << (endOrdered - startOrdered) / (float)CLOCKS_PER_SEC << " seconds"
-                 << endl;
             cout << "Time of Unordered Map: "
                  << (endUnordered - startUnordered) / (float)CLOCKS_PER_SEC
                  << " seconds" << endl;
+            cout << "Time of Ordered Map: "
+                 << (endOrdered - startOrdered) / (float)CLOCKS_PER_SEC << " seconds"
+                 << endl;
             menu = false;
-
             break;
         }
-        case 5:
-            double minPrecipitation, maxPrecipitation;
-            cout << "What is the minimum precipitation you desire?" << endl;
-            cin >> minPrecipitation;
-            cout << "What is the maximum precipitation you desire?" << endl;
-            cin >> maxPrecipitation;
-
-            startOrdered = clock();
-            ordered.precipitation(minPrecipitation, maxPrecipitation);
-            endOrdered = clock();
-
-            startUnordered = clock();
-            unordered.windSpd(minPrecipitation, maxPrecipitation);
-            endUnordered = clock();
-
-            cout << "Time of Ordered Map: "
-                 << (endOrdered - startOrdered) / (float)CLOCKS_PER_SEC << " seconds"
-                 << endl;
-            cout << "Time of Unordered Map: "
-                 << (endUnordered - startUnordered) / (float)CLOCKS_PER_SEC
-                 << " seconds" << endl;
-            menu = false;
-            break;
         case 6:
             cout << "Thank you for using Weather Sort!" << endl;
             menu = false;
